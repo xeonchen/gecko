Floorp-Projects/Floorp

Bug 1508064 - Simplify JS_FOR_PROTOTYPES. r=jandem
Add JS_FOR_PROTOTYPES_ macro that takes REAL_IF_SAB, etc helpers to
handle conditional proto keys. This is easier to read and avoids macro
expansion issues confusing clang-format.

Differential Revision: https://phabricator.services.mozilla.com/D12238

--HG--
extra : moz-landing-system : lando

js/public/ProtoKey.h
@@ -54,7 +54,8 @@
 #define IF_SAB(REAL,IMAGINARY) IMAGINARY
 #endif
 
-#define JS_FOR_PROTOTYPES(REAL,IMAGINARY) \
+#define JS_FOR_PROTOTYPES_(REAL, IMAGINARY, \
+                           REAL_IF_INTL, REAL_IF_BDATA, REAL_IF_SAB, REAL_IF_BIGINT) \
     IMAGINARY(Null,             InitNullClass,          dummy) \
     REAL(Object,                InitViaClassSpec,       OCLASP(Plain)) \
     REAL(Function,              InitViaClassSpec,       &JSFunction::class_) \
@@ -88,20 +89,20 @@
     REAL(Float32Array,          InitViaClassSpec,       TYPED_ARRAY_CLASP(Float32)) \
     REAL(Float64Array,          InitViaClassSpec,       TYPED_ARRAY_CLASP(Float64)) \
     REAL(Uint8ClampedArray,     InitViaClassSpec,       TYPED_ARRAY_CLASP(Uint8Clamped)) \
-IF_BIGINT(REAL,IMAGINARY)(BigInt, InitViaClassSpec, OCLASP(BigInt)) \
+    REAL_IF_BIGINT(BigInt, InitViaClassSpec, OCLASP(BigInt)) \
     REAL(Proxy,                 InitProxyClass,         &js::ProxyClass) \
     REAL(WeakMap,               InitWeakMapClass,       OCLASP(WeakMap)) \
     REAL(Map,                   InitViaClassSpec,       OCLASP(Map)) \
     REAL(Set,                   InitViaClassSpec,       OCLASP(Set)) \
     REAL(DataView,              InitViaClassSpec,       OCLASP(DataView)) \
     REAL(Symbol,                InitSymbolClass,        OCLASP(Symbol)) \
-IF_SAB(REAL,IMAGINARY)(SharedArrayBuffer,       InitViaClassSpec, OCLASP(SharedArrayBuffer)) \
-IF_INTL(REAL,IMAGINARY) (Intl,                  InitIntlClass,          CLASP(Intl)) \
-IF_BDATA(REAL,IMAGINARY)(TypedObject,           InitTypedObjectModuleObject,   OCLASP(TypedObjectModule)) \
+    REAL_IF_SAB(SharedArrayBuffer, InitViaClassSpec, OCLASP(SharedArrayBuffer)) \
+    REAL_IF_INTL(Intl, InitIntlClass, CLASP(Intl)) \
+    REAL_IF_BDATA(TypedObject, InitTypedObjectModuleObject, OCLASP(TypedObjectModule)) \
     REAL(Reflect,               InitReflect,            nullptr) \
     REAL(WeakSet,               InitWeakSetClass,       OCLASP(WeakSet)) \
     REAL(TypedArray,            InitViaClassSpec,       &js::TypedArrayObject::sharedTypedArrayPrototypeClass) \
-IF_SAB(REAL,IMAGINARY)(Atomics, InitAtomicsClass, OCLASP(Atomics)) \
+    REAL_IF_SAB(Atomics, InitAtomicsClass, OCLASP(Atomics)) \
     REAL(SavedFrame,            InitViaClassSpec,       &js::SavedFrame::class_) \
     REAL(Promise,               InitViaClassSpec,       OCLASP(Promise)) \
     REAL(ReadableStream,        InitViaClassSpec,       &js::ReadableStream::class_) \
@@ -119,6 +120,14 @@ IF_SAB(REAL,IMAGINARY)(Atomics, InitAtomicsClass, OCLASP(Atomics)) \
     IMAGINARY(WasmMemory,       dummy,                  dummy) \
     IMAGINARY(WasmTable,        dummy,                  dummy) \
     IMAGINARY(WasmGlobal,       dummy,                  dummy) \
+
+#define JS_FOR_PROTOTYPES(REAL, IMAGINARY) \
+    JS_FOR_PROTOTYPES_(REAL, \
+                       IMAGINARY, \
+                       IF_INTL(REAL, IMAGINARY), \
+                       IF_BDATA(REAL, IMAGINARY), \
+                       IF_SAB(REAL, IMAGINARY), \
+                       IF_BIGINT(REAL, IMAGINARY))
 
 #define JS_FOR_EACH_PROTOTYPE(MACRO) JS_FOR_PROTOTYPES(MACRO,MACRO)
 

xeonchen/gecko

js/public/ProtoKey.h
@@ -60,7 +60,8 @@
 #define IF_SAB(real,imaginary) imaginary
 #endif
 
-#define JS_FOR_PROTOTYPES(real,imaginary) \
+#define JS_FOR_PROTOTYPES_(real, imaginary, \
+                           REAL_IF_INTL, REAL_IF_BDATA, REAL_IF_SAB, REAL_IF_SIMD) \
     imaginary(Null,             InitNullClass,          dummy) \
     real(Object,                InitViaClassSpec,       OCLASP(Plain)) \
     real(Function,              InitViaClassSpec,       &JSFunction::class_) \
@@ -101,14 +102,14 @@
     real(Set,                   InitViaClassSpec,       OCLASP(Set)) \
     real(DataView,              InitViaClassSpec,       OCLASP(DataView)) \
     real(Symbol,                InitSymbolClass,        OCLASP(Symbol)) \
-IF_SAB(real,imaginary)(SharedArrayBuffer,       InitViaClassSpec, OCLASP(SharedArrayBuffer)) \
-IF_INTL(real,imaginary) (Intl,                  InitIntlClass,          CLASP(Intl)) \
-IF_BDATA(real,imaginary)(TypedObject,           InitTypedObjectModuleObject,   OCLASP(TypedObjectModule)) \
+    REAL_IF_SAB(SharedArrayBuffer, InitViaClassSpec, OCLASP(SharedArrayBuffer)) \
+    REAL_IF_INTL(Intl, InitIntlClass, CLASP(Intl)) \
+    REAL_IF_BDATA(TypedObject, InitTypedObjectModuleObject, OCLASP(TypedObjectModule)) \
     real(Reflect,               InitReflect,            nullptr) \
-IF_SIMD(real,imaginary)(SIMD,                   InitSimdClass, OCLASP(Simd)) \
+    REAL_IF_SIMD(SIMD, InitSimdClass, OCLASP(Simd)) \
     real(WeakSet,               InitWeakSetClass,       OCLASP(WeakSet)) \
     real(TypedArray,            InitViaClassSpec,       &js::TypedArrayObject::sharedTypedArrayPrototypeClass) \
-IF_SAB(real,imaginary)(Atomics, InitAtomicsClass, OCLASP(Atomics)) \
+    REAL_IF_SAB(Atomics, InitAtomicsClass, OCLASP(Atomics)) \
     real(SavedFrame,            InitViaClassSpec,       &js::SavedFrame::class_) \
     real(Promise,               InitViaClassSpec,       OCLASP(Promise)) \
     real(ReadableStream,        InitViaClassSpec,       &js::ReadableStream::class_) \
@@ -128,6 +129,14 @@ IF_SAB(real,imaginary)(Atomics, InitAtomicsClass, OCLASP(Atomics)) \
     imaginary(WasmMemory,       dummy,                  dummy) \
     imaginary(WasmTable,        dummy,                  dummy) \
     imaginary(WasmGlobal,       dummy,                  dummy) \
+
+#define JS_FOR_PROTOTYPES(REAL, IMAGINARY) \
+    JS_FOR_PROTOTYPES_(REAL, \
+                       IMAGINARY, \
+                       IF_INTL(REAL, IMAGINARY), \
+                       IF_BDATA(REAL, IMAGINARY), \
+                       IF_SAB(REAL, IMAGINARY), \
+                       IF_SIMD(REAL, IMAGINARY))
 
 #define JS_FOR_EACH_PROTOTYPE(macro) JS_FOR_PROTOTYPES(macro,macro)
 